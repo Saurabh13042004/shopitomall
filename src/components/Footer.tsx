832abe60b406dc08d205fa55e0c94847f4b0fc49
Saurabh Shukla <saurabh1258.be21@chitkarauniversity.edu.in>
2024-12-10 22:49:57 +0530

    footer-cahnges

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,15 +11,25 @@ export function Footer() {
       <div className="container mx-auto px-6 text-center">
         <Logo />
         
-        <div className="mt-8 text-sm text-gray-600 dark:text-gray-400">
-          <p>&copy; 2024 ShopItToMall - All rights reserved</p>
-          <div className="mt-2 space-x-4">
-            <a href="#" className="hover:text-red-500">Privacy Policy</a>
-            <span>•</span>
-            <a href="#" className="hover:text-red-500">Cookie Policy</a>
-          </div>
-          <p className="mt-2">Powered by ShopItToMall</p>
-        </div>
+        <div className="mt-8 text-sm text-gray-600 dark:text-gray-400 flex flex-wrap items-center align-middle text-center justify-center gap-8 space-y-2 md:space-y-0">
+  <p>&copy; 2024 ShopItToMall - All rights reserved</p>
+  
+  <div className="flex items-center space-x-4">
+  <span>|</span>
+    <a href="#" className="hover:text-red-500">Privacy Policy</a>
+    <span>|</span>
+    <a href="#" className="hover:text-red-500">Cookie Policy</a>
+    <span>|</span>
+  </div>
+  
+  <p className="flex items-center space-x-2">
+    Powered by 
+    <img src="./logosecond.jpg" alt="ShopItToMall Logo" className="h-6 inline-block" />
+   
+  </p>
+</div>
+
+
       </div>
     </motion.footer>
   );
